4bdb1380319d2f7b67bde64f0652e4ad6e2183ca
Jay-el C. Oconer <jay-el_oconer@dlsl.edu.ph>
2024-08-30 20:52:05 +0800

    Final Commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <string>
-#include <vector>  // No fixed array size, used emplace.back and .empty
+#include <string> 
+#include <vector> // No fixed array size, used emplace.back and .empty
 #include <iomanip> // setw
-#include <cctype>  // Auto Upper case the lower case for searching
 
 // Classes and Objects For Adding Books
 class Book
@@ -12,7 +11,7 @@ private:
     std::string author;
     int year;
 
-public: // Constructor and Destructor and Getter
+public: // Constructor and Destructor
     Book(std::string t, std::string a, int y) : title(t), author(a), year(y)
     {
         std::cout << "Resource Book added successfully!" << std::endl;
@@ -20,8 +19,6 @@ public: // Constructor and Destructor and Getter
     ~Book()
     {
         std::cout << "Resource Book released!" << std::endl;
-        system("cls");
-        std::cout << "Exited the program." << std::endl;
     }
     std::string getTitle() const { return title; }
     std::string getAuthor() const { return author; }
@@ -38,14 +35,6 @@ public:
     Library() = default;
     ~Library() = default;
 
-    void ConvertUpper(std::string &title) const
-    {
-        for (int i = 0; i < title.length(); i++)
-        {
-            title[i] = toupper(title[i]);
-        }
-    }
-    //Add of title, author, year
     void AddBook()
     {
         std::string title, author;
@@ -54,15 +43,6 @@ public:
         std::cout << "Enter Book Title: ";
         std::cin.ignore();
         std::getline(std::cin, title);
-        ConvertUpper(title);
-        for (const Book &book : books)
-        {
-            if (book.getTitle() == title)
-            {
-                std::cout << "Book already exists." << std::endl;
-                return;
-            }
-        }
 
         std::cout << "Enter Book Author: ";
         std::getline(std::cin, author);
@@ -73,7 +53,7 @@ public:
         books.emplace_back(title, author, year);
         std::cout << "Book added successfully!" << std::endl;
     }
-//data of books
+
     void BooksLibrary() const
     {
         if (books.empty())
@@ -93,19 +73,18 @@ public:
             }
         }
     }
-//searching for books
+
     void SearchBook() const
     {
-        std::string title;
+        std::string searchTitle;
         std::cout << "Enter the title of the book to search: ";
         std::cin.ignore(); // Clear the input buffer
-        std::getline(std::cin, title);
-        ConvertUpper(title);
+        std::getline(std::cin, searchTitle);
 
         bool found = false;
         for (const Book &book : books)
         {
-            if (book.getTitle() == title)
+            if (book.getTitle() == searchTitle)
             {
                 std::cout << "Book found!" << std::endl;
                 std::cout << "Title: " << book.getTitle() << std::endl;
@@ -122,47 +101,38 @@ public:
         }
     }
 };
-//Main Menu
+
 void Menu(Library &lib)
 {
     int bookchoice;
     while (true)
     {
-        std::cout << "\n"
-                  << "Menu" << std::endl;
-        std::cout << "1 - Add a Book" << std::endl;
-        std::cout << "2 - Display Books" << std::endl;
-        std::cout << "3 - Search a Book" << std::endl;
-        std::cout << "4 - Exit." << std::endl;
+        std::cout << "Enter '1' to Add a Book, Enter '2' to Display Books, Enter '3' to Search a Book, Enter '4' to Exit." << std::endl;
         std::cin >> bookchoice;
 
         switch (bookchoice)
         {
         case 1:
-            system("cls");
             lib.AddBook();
             break;
         case 2:
-            system("cls");
             lib.BooksLibrary();
             break;
         case 3:
-            system("cls");
             lib.SearchBook();
             break;
         case 4:
-            system("cls");
+            std::cout << "Exited the program." << std::endl;
             return;
         default:
-            system("cls");
             std::cout << "Invalid selection, please try again." << std::endl;
-            std::cin.clear();
             break;
         }
     }
 }
 
-int main() {
+int main()
+{
     Library lib;
     Menu(lib);
     return 0;
